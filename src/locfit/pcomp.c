785971fd0c6ff6bcfe5b3314215fbd64f4762cee
Cole Trapnell <coletrap@uw.edu>
2011-03-19 16:14:27 +0000

    a little more debug code

--- src/locfit/pcomp.c
+++ src/locfit/pcomp.c
@@ -89,6 +89,9 @@ INT nopc;
     lf->mi[MPC] = 1;
     des->xev = pc->xbar;
     k = locfit(lf,des,0.0,0);
+#if DEBUG
+    fprintf(stderr, "Fitting...\n");
+#endif
     if (lf_error) return;
     switch(k)
     { case LF_NOPT:
