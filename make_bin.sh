e66e16593436455a3e6e3203851206dde8e8075a
Geo Pertea <gpertea@jhu.edu>
2012-05-30 16:35:10 +0000

    make_bin.sh now links libstdc++ statically (requires gcc 4.5+); LDFLAGS is no longer ignored

--- make_bin.sh
+++ make_bin.sh
@@ -1,12 +1,36 @@
-#!/bin/bash
-
+#!/bin/sh
 #simple script to pack up a precompiled binary package, with the boost thread
 # library statically linked in.
+if [[ -z "$2" ]]; then
+ echo -e "Usage:\n./make_bin.sh <package_base_name> <boost_prefix> [<bam_prefix> [<Eigen_prefix>]]"
+ exit 1
+fi
 
 echo "packing up $1.tar.gz, using boost in $2, linking against $3 and using BAM in $4, using Eigen in $5"
 mkdir $1
-make clean
-./configure --enable-intel64 --with-boost=$2 --with-boost-thread=$3 --with-bam=$4 --with-eigen=$5
+#make clean
+make distclean
+if [[ $(uname -m) = "x86_64" ]]; then
+ echo "Linking statically on x86_64 (only for gcc 4.5+).."
+ export LDFLAGS="-static-libgcc -static-libstdc++"
+fi 
+l2="$2"
+l3="$3"
+if [[ -z "$l3" ]]; then
+  l3="$l2"
+fi
+l4="$4"
+if [[ -z "$l4" ]]; then
+  l4="$l2"
+fi
+# l5="$5"
+# if [[ -z "$l5" ]]; then
+#   l5="$l2"
+# fi
+
+
+#./configure --enable-intel64 --with-boost=$l2 --with-boost-thread=$l3 --with-bam=$l4 --with-eigen=$l5
+./configure --enable-intel64 --with-boost=$l2 --with-bam=$l3 --with-eigen=$l4
 make
 cp src/cufflinks $1
 cp src/cuffcompare $1
